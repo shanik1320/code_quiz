f421e4c45fb705ecccc680227bb68e655b4f1422
shanik hernandez <shaniik.her93@gmail.com>
2020-03-16 22:28:22 -0400

    added 2 files

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+var questions = [
+    {
+      title: "Example Question 1:",
+      choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
+      answer: "answer from choices"
+    },
+    {
+      title: "Example Question 2:",
+      choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
+      answer: "answer from choices"
+    }
+  ];
