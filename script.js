982efa6b961660a77bb6f4c80e6c242ca33a10e3
shanik hernandez <shaniik.her93@gmail.com>
2020-03-17 15:20:32 -0400

    added style file

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
-// make variables 
-//make an array for the questions and answers
+// global variables
+var 
+
+
+
+timer = 0
 // make buttons to submit answers
-// select the right answer
+
 // keep track of the right questions
 
 
+console.log(questions)
 
-
-
+//make an array for the questions and answers
 var questions = [
     {
-      title: "Example Question 1:",
-      choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
-      answer: "answer from choices"
+      title: "Question 1",
+      q : ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
+      // select the right answer
+      a: "answer from choices"
     },
     {
       title: "Example Question 2:",
-      choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
-      answer: "answer from choices"
+      q: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
+      a: "answer from choices"
     }
   ];
+
+
+
+  // for loop to get the questions random
+  for (var index = 0; index < questions.length; index++) {
+    
+      
+  }
